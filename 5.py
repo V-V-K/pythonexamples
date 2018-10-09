bc00907b739f2dc81e75fff15910cbc51e97485c
Vamsi Krishna <33099230+V-V-K@users.noreply.github.com>
2018-10-09 21:13:21 +0530

    Update 5.py
    Hey, the program you wrote was in C++. But no worries, I have got the code up and running in Python. 
    Cheers.

--- 5.py
+++ 5.py
@@ -1,25 +1,13 @@
-#include <iostream>
-using namespace std;
+#Armstrong number check upto 3 digits only
+origNum = int(input("Enter a positive  integer: "))
+num = origNum;
+sum=0
+while(num != 0):
+    rem = num % 10
+    sum += rem ** 3
+    num //= 10
 
-int main()
-{
-  int origNum, num, rem, sum = 0;
-  cout << "Enter a positive  integer: ";
-  cin >> origNum;
-
-  num = origNum;
-
-  while(num != 0)
-  {
-      rem = num % 10;
-      sum += rem * rem * rem;
-      num /= 10;
-  }
-
-  if(sum == origNum)
-    cout << origNum << " is an Armstrong number.";
-  else
-    cout << origNum << " is not an Armstrong number.";
-
-  return 0;
-}
+if(sum == origNum):
+    print(origNum," is an Armstrong number.")
+else:
+    print(origNum," is not an Armstrong number.")
